9fb7ac5bc83356b55c7ae712852b2733f86541a3
sonya287 <74300825+sonya287@users.noreply.github.com>
2021-07-21 17:19:48 +0300

    Updates

---
+++ static/js/main.b56a0deb.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpgoit-react-hw-02-feedback"]=this["webpackJsonpgoit-react-hw-02-feedback"]||[]).push([[0],{11:function(e,t,c){"use strict";c.r(t);var n=c(1),a=c.n(n),r=c(3),s=c.n(r),i=(c(8),c(9),c(0));var o=function(){return Object(i.jsx)("div",{className:"App",children:Object(i.jsx)("header",{className:"App-header"})})};s.a.render(Object(i.jsx)(a.a.StrictMode,{children:Object(i.jsx)(o,{})}),document.getElementById("root"))},8:function(e,t,c){},9:function(e,t,c){}},[[11,1,2]]]);
+//# sourceMappingURL=main.b56a0deb.chunk.js.map
